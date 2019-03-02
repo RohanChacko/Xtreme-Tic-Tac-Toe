1d7d1d76d70b9980957b137335dfa436d6015fd2
rohanchacko007 <rohanchacko007@gitlab.com>
2019-03-02 23:15:07 +0530

    Improved open move heuristic

--- trials/minimax.py
+++ trials/minimax.py
@@ -64,6 +64,24 @@ class MinimaxAgent(object):
                 for j in range(0,9):
                     self.zobristboard[k][i].append( [random.randint(1, 2**64 - 1) for l in range(2)] )
 
+        self.player_smallboard_count = []
+
+        # Setting no. of x's & o's to be zero initially for each smallboard.
+        # Each cell [num(x) num(o)]
+        for k in range(0,2):
+            self.player_smallboard_count.append([])
+            for i in range(0,3):
+                self.player_smallboard_count[k].append([])
+                for j in range(0,3):
+                    self.player_smallboard_count[k][i].append(0)
+
+        self.num_moves = 0
+        self.max_1 = -1*sys.maxint
+        self.max_2 = -1*sys.maxint - 1
+
+        self.good_board_1 = [-1,-1,-1]
+        self.good_board_2 = [-1,-1,-1]
+
         if player == 'x':
             self.opponent = 'o'
         else:
@@ -91,7 +109,6 @@ class MinimaxAgent(object):
             zobhash = self.computeZobHash(self.board.big_boards_status)
             if zobhash in self.transposition_table:
                 state_score = self.transposition_table[zobhash]
-
             else:
                 state_score = self.evaluate_heuristic(self.board, old_move)
                 self.transposition_table[zobhash] = state_score
@@ -124,12 +141,11 @@ class MinimaxAgent(object):
         else:
             best_heuristic_val = sys.maxint
             available_moves = self.board.find_valid_move_cells(old_move)
-            for current_move in available_moves:
 
+            for current_move in available_moves:
 
                 # copy_board.big_boards_status[current_move[0]][current_move[1]][current_move[2]] = self.opponent
                 self.board.update(old_move, current_move, self.opponent)
-
 
                 allowed_small_board = [current_move[1]%3, current_move[2]%3]
                 if self.board.small_boards_status[0][allowed_small_board[0]][allowed_small_board[1]] != '-' and self.board.small_boards_status[1][allowed_small_board[0]][allowed_small_board[1]] != '-':
@@ -155,15 +171,56 @@ class MinimaxAgent(object):
         best_move = (-1,-1,-1)
         available_moves = self.board.find_valid_move_cells(old_move)
 
+        if old_move != (-1,-1,-1):
+            self.num_moves +=1
+
         if len(available_moves) == 1:
             return available_moves[0]
 
+        # For open moves
+        elif len(available_moves) > 18 and self.num_moves > 0:
+
+            # Reset values
+            for k in range(0,2):
+                for i in range(0,3):
+                    for j in range(0,3):
+                        self.player_smallboard_count[k][i][j] = 0
+
+            min_1 = sys.maxint
+            min_2 = sys.maxint
+            avail = []
+            self.good_board_1 = [-1,-1,-1]
+            self.good_board_2 = [-1,-1,-1]
+
+            for move in available_moves:
+                self.player_smallboard_count[move[0]][move[1]%3][move[2]%3] +=1
+
+            for k in range (0,2):
+                for i in range(0,3):
+                    for j in range(0,3):
+
+                        if min_1 > self.player_smallboard_count[k][i][j] and self.player_smallboard_count[k][i][j] > 0:
+                            min_2 = min_1
+                            self.good_board_2 = self.good_board_1
+                            min_1 = self.player_smallboard_count[k][i][j]
+                            self.good_board_1 = [k, i, j]
+
+            for move in available_moves:
+                if [move[0], move[1]%3, move[2]%3] == self.good_board_1 or [move[0], move[1]%3, move[2]%3] == self.good_board_2:
+                    avail.append(move)
+
+            # print self.good_board_1
+            # print self.good_board_2
+            print "Open move"
+            available_moves = avail
         for current_move in available_moves:
 
             # print(str(current_move))
 
             # Max Player Made His Move
             board_copy = deepcopy(self.board)
+
+            # IMPLEMENT OWN UPDATE FN. USELESS COMPUTATIONS HAPPENING
             self.board.update(old_move, current_move, self.player)
             # copy_board.big_boards_status[current_move[0]][current_move[1]][current_move[2]] = self.player
 
@@ -184,7 +241,7 @@ class MinimaxAgent(object):
                 best_move = current_move
                 best_move_val = move_value
 
-
+        self.num_moves +=1
         print self.player + str(best_move)
         return best_move
 
@@ -195,13 +252,17 @@ class MinimaxAgent(object):
         #################################### Heuristic A ####################################
 
         if board.find_terminal_state() == ('x','WON') and self.player == 'x':
-            state_score += 100
+            state_score += 500
+            return sys.maxint
         elif  board.find_terminal_state() == ('o','WON') and self.player == 'o':
-            state_score += 100
+            state_score += 500
+            return sys.maxint
         elif board.find_terminal_state() == ('x','WON') and self.player == 'o':
-            state_score -= 100
+            state_score -= 500
+            return -1*sys.maxint
         elif board.find_terminal_state() == ('o','WON') and self.player == 'x':
-            state_score -= 100
+            state_score -= 500
+            return -1*sys.maxint
 
 
         #################################### Heuristic B ####################################
@@ -215,7 +276,7 @@ class MinimaxAgent(object):
                     if self.board.small_boards_status[k][i][j] == self.player:
                         state_score += 5*self.small_board_status_weights[k][i][j]
                     if self.board.small_boards_status[k][i][j] == self.opponent:
-                        state_score -= self.small_board_status_weights[k][i][j]
+                        state_score -= 2*self.small_board_status_weights[k][i][j]
         #################################### Heuristic C ####################################
 
         # The Special Positions of the Small Board of the Big Board Positions
@@ -224,17 +285,6 @@ class MinimaxAgent(object):
                 for j in range(9):
                     if self.board.big_boards_status[k][i][j] == self.player:
                         state_score += self.small_board_weights[k][i][j]
-
-
-        #################################### Heuristic D ####################################
-
-        # Possibility of making a winning position
-		# for k in range(2):
-		# 	if self.board.small_boards_status[k][old_move[0]][old_move[1]] == "-":
-		# 		for i in range(3*old_move[0], 3*old_move[0]+3):
-		# 			for j in range(3*old_move[1], 3*old_move[1]+3):
-
-
 
 
         return state_score
